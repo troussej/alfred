b3af34043c361010c8f57137248bc40d5cb4bc40
troussej <trousset.joel@gmail.com>
2016-12-26 18:09:27 +0100

    replace ts by js for server

---
+++ portal/server/app.js
@@ -0,0 +1,33 @@
+"use strict";
+const express = require('express');
+const body_parser_1 = require('body-parser');
+const path = require('path');
+const compression = require('compression');
+const light_1 = require('./routes/light');
+const app = express();
+exports.app = app;
+app.disable('x-powered-by');
+app.use(body_parser_1.json());
+app.use(compression());
+app.use(body_parser_1.urlencoded({ extended: true }));
+// api routes
+app.use('/api/hue', light_1.lightRouter);
+if (app.get('env') === 'production') {
+    // in production mode run application from dist folder
+    app.use(express.static(path.join(__dirname, '/../client')));
+}
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    next(err);
+});
+// production error handler
+// no stacktrace leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        error: {},
+        message: err.message
+    });
+});
+//# sourceMappingURL=E:/workspace/alfred/portal/dist/server/app.js.map
---
+++ portal/server/routes/light.js
@@ -0,0 +1,24 @@
+"use strict";
+const express_1 = require('express');
+const Hueook = require('jtro-hueook');
+const lightRouter = express_1.Router();
+exports.lightRouter = lightRouter;
+const hueook = new Hueook();
+lightRouter.route('light').get((request, response) => {
+    hueook.getLights().then(lights => { response.json(lights); });
+});
+lightRouter
+    .route('/light/:id')
+    .get((request, response) => {
+    hueook.getLight(request.params.id).then(lights => { response.json(lights); });
+})
+    .put((request, response) => {
+    hueook.saveLight(request.params.id, request.body).then(lights => { response.json(lights); });
+});
+lightRouter.get('/group/:id', (request, response) => {
+    hueook.getGroup(request.params.id).then(res => { response.json(res); });
+});
+lightRouter.get('/group', (request, response) => {
+    hueook.getGroups().then(res => { response.json(res); });
+});
+//# sourceMappingURL=E:/workspace/alfred/portal/dist/server/routes/light.js.map
